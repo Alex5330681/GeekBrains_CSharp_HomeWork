65c0ae199943f2d2269b0d32eeeaa32412003ba3
Alex White <5330681@gmail.com>
2020-04-12 17:52:28 +0300

    Задание 1 и 2

---
+++ Lesson_5_HomeWork/Message.cs
@@ -0,0 +1,127 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+
+namespace Task2_messageClass
+{
+    public static class Message
+{
+
+    static string[] stringToArray(string msg)
+    {
+        Regex regex = new Regex(@"\w+");
+        string[] msgArray = new string[regex.Matches(msg).Count];
+        int i = 0;
+        foreach (Match word in regex.Matches(msg))
+        {
+            msgArray[i] = word.ToString();
+            i++;
+        }
+        return msgArray;
+    }
+
+   public static void CustomWord(string msg, int wordLength)
+    {
+        string[] msgArray = stringToArray(msg);
+
+        for (int i = 0; i < msgArray.Length; i++)
+        {
+            if (msgArray[i].Length <= wordLength)
+            {
+                Console.Write($"{msgArray[i]} ");
+            }
+        }
+        Console.WriteLine();
+    }
+
+   public static string WordRemove (string msg, char symbol)
+    {
+        string[] msgArray = stringToArray(msg);
+        string newMSg = "";
+
+        for (int i = 0; i < msgArray.Length; i++)
+        {
+            if (msgArray[i][msgArray[i].Length - 1] != symbol)
+            {
+                newMSg = newMSg + msgArray[i] + " ";
+            }
+        }
+        newMSg.TrimEnd(' ');
+        return newMSg;
+    }
+
+    public static void WordRemove(ref string msg, char symbol)
+    {
+        string[] msgArray = stringToArray(msg);
+        for (int i = 0; i < msgArray.Length; i++)
+        {
+            if (msgArray[i][msgArray[i].Length - 1] == symbol)
+            {
+                msg.Replace(msgArray[i], "");
+            }
+        }
+    }
+
+    public static string MaxWord(string msg)
+    {
+        string[] msgArray = stringToArray(msg);
+        int max = msgArray[0].Length;
+        string word = msgArray[0];
+
+        for (int i = 1; i < msgArray.Length; i++)
+        {
+            if (msgArray[i].Length > max)
+            {
+                max = msgArray[i].Length;
+                word = msgArray[i];
+            }
+        }
+        return word;
+    }
+
+   public static string MaxWordString(string msg)
+    {
+        StringBuilder maxWords = new StringBuilder();
+        string[] msgArray = stringToArray(msg);
+        int max = msgArray[0].Length;
+        for (int i = 1; i < msgArray.Length; i++)
+        {
+            if (msgArray[i].Length > max)
+            {
+                max = msgArray[i].Length;
+            }
+        }
+        for (int i = 0; i < msgArray.Length; i++)
+        {
+            if (msgArray[i].Length == max)
+            {
+                maxWords.Append(msgArray[i]);
+                maxWords.Append(" ");
+            }
+        }
+        return maxWords.ToString().TrimEnd(' ');
+    }
+
+   public static void TextAnalysis(string[] array, string text)
+    {
+        Dictionary<string, int> dict = new Dictionary<string, int>();
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            Regex regex = new Regex(array[i]);
+            dict[array[i]] = regex.Matches(text.ToLower()).Count;
+        }
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine($"{array[i]} - {dict[array[i]]}");
+        }
+    }
+
+}
+}
+
+
---
+++ Lesson_5_HomeWork/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Task2_messageClass;
+
+
+/*
+ЗАДАНИЕ №1
+Создать программу, которая будет проверять корректность ввода логина.
+Корректным логином будет строка от 2 до 10 символов, 
+содержащая только буквы латинского алфавита или цифры, при этом цифра не может быть первой
+*/
+
+//namespace Lesson_5_HomeWork
+//{
+//    class Program
+//    {
+//        static void Main()
+//        {
+
+          
+
+//            bool b1;
+
+//            Console.WriteLine(
+//                "Придумайте логин и пароль, \n от 2 до 8 символов \n запрещено в начале использовать цифры ");
+//            Console.Write("логин >");
+//            string Login = Console.ReadLine();
+//            Console.Write("пароль >");
+//            string Pass = Console.ReadLine();
+
+//            Regex RegNum = new Regex(@"[^A-Za-z]");
+
+//            if (Login.Length < 2 || Pass.Length > 8)
+//            {
+//                Console.WriteLine("Неправльное количество символов");
+//                b1 = false;
+//            }
+
+//            if (RegNum.IsMatch(Login.Substring(0, 1)) || RegNum.IsMatch(Pass.Substring(0, 1)))
+
+//            {
+//                b1 = false;
+//                Console.WriteLine("Логин или пароль должен начинаться с буквы");
+//            }
+
+//            else
+//            {
+//                Console.WriteLine("Отличный пароль");
+//                b1 = true;
+//            }
+
+//            Console.WriteLine($"Результат {b1}");
+//            Console.ReadKey();
+//        }
+//    }
+
+
+    /////////////////////////////////////////////////////////////////////////////////////////
+
+    //    /*
+    //        ЗАДАНИЕ №2
+    //        Разработать класс Message, содержащий следующие статические методы для обработки
+    //        текста:
+    //        а) Вывести только те слова сообщения, которые содержат не более n букв.
+    //        б) Удалить из сообщения все слова, которые заканчиваются на заданный символ.
+    //        в) Найти самое длинное слово сообщения.
+    //        г) Сформировать строку с помощью StringBuilder из самых длинных слов сообщения.
+    //    */
+
+
+    namespace Lesson_5_HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Три кита как три кота держат землю не спроста" +
+                          " Кот котан и кит с китенком, три, три, три на землю три ";
+
+            Console.WriteLine($"Исходный текст:\n{text}");
+
+            Console.WriteLine("\nа) Вывести только те слова сообщения, которые содержат не более 3 букв:");
+            Message.CustomWord(text, 3);
+
+            Console.WriteLine($"\nб) Удалить из сообщения все слова, которые заканчиваются " +
+                              $"на заданный символ (а):\n{Message.WordRemove(text, 'а')}");
+
+            Console.WriteLine($"\nв) Найти самое длинное слово сообщения:\n{Message.MaxWord(text)}");
+            Console.WriteLine($"\nг) Сформировать строку с помощью StringBuilder из самых " +
+                              $"длинных слов сообщения:\n{Message.MaxWordString(text)}");
+
+            string[] array = { "кот", "кит", "три", "землю" };
+            Console.WriteLine("\n***Создать метод, который производит частотный анализ текста:");
+
+            Message.TextAnalysis(array, text);
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
+
+
+
